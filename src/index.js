7720af01feb580e0f71e4baa08cc29060b98e372
LifeAsDev <angelosarmiento5d@gmail.com>
2025-02-14 10:33:00 -0400

    update webpack output files

--- src/index.js
+++ src/index.js
@@ -4,3 +4,72 @@ console.log("Buffer en Base64:", miBuffer.toString('base64'));
 window.Buffer = require('buffer').Buffer; // Define Buffer globalmente
 
 // Puedes seguir usando Buffer como lo harías en Node.js aquí...
+const solConnect = new window.SolanaConnect();
+
+async function connectWallet() {
+    if (solConnect) solConnect.openMenu();
+}
+function check() {
+    if (solConnect) {
+        console.log(solConnect.activeWallet);
+        const wallet = solConnect.getWallet();
+        sendTransaction(
+            wallet,
+            "HeMuWnMKPdrgkTay1swBP3WLd3eeZDWUy4PYPCPkvVDQ",
+            0.001
+        );
+    }
+}
+const sendTransaction = async (
+    wallet, // La wallet conectada (Phantom, Solflare, etc.)
+    recipient, // Dirección del destinatario
+    amount
+) => {
+    const { Connection, PublicKey, SystemProgram, Transaction } =
+        solanaWeb3;
+    const devnet = "https://api.devnet.solana.com";
+    const mainnet =
+        "https://solana-mainnet.g.alchemy.com/v2/su4NQiUu5uSE_3oMj-_riW_gHXqQPECJ";
+    if (!wallet || !wallet.publicKey) {
+        console.error("No hay una wallet conectada");
+        return;
+    }
+    console.log({ wallet });
+    const connection = new Connection(devnet, "confirmed");
+
+    const transaction = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: wallet.publicKey,
+            toPubkey: new PublicKey(recipient),
+            lamports: amount * 10 ** 9, // Convertir SOL a lamports
+        })
+    );
+
+    try {
+        const { blockhash } = await connection.getLatestBlockhash();
+
+        // 2. Asignar el recentBlockhash a la transacción
+        transaction.recentBlockhash = blockhash;
+        transaction.feePayer = wallet.publicKey; // Establecer el fee payer (¡Importante!)
+
+        // 3. Firmar la transacción
+        const signedTransaction = await wallet.signTransaction(transaction);
+
+        // 4. Serializar la transacción firmada
+        const serializedTransaction = signedTransaction.serialize();
+
+        // 5. Enviar la transacción
+        const { signature } = await connection.sendRawTransaction(
+            serializedTransaction
+        );
+
+        console.log("Transacción enviada:", signature);
+    } catch (error) {
+        console.error("Error enviando la transacción:", error);
+    }
+};
+
+function set() {
+    const buf = Buffer.from("Hola, mundo!", "utf-8");
+    console.log(buf.toString("hex"));
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,50 @@
-const path = require('path');
+const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development', // o 'production' para el entorno de producción
-    entry: './src/index.js', // Ruta al archivo principal de tu aplicación
-    output: {
-        path: path.resolve(__dirname, 'dist'), // Ruta absoluta a la carpeta de salida
-        filename: 'main.js', // Nombre del archivo de salida
-        publicPath: '/', // Ruta pública para el navegador (importante para dev-server)
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'), // Ruta a la carpeta de archivos estáticos (index.html)
-            publicPath: '/', // La ruta pública para acceder a los archivos estáticos
-        },
-        port: 8080,
-        hot: true, // Habilita la recarga en caliente (opcional, pero recomendado)
-    },
-    module: {  // Para procesar diferentes tipos de archivos (CSS, imágenes, etc.)
-        rules: [
-            // Ejemplo para procesar archivos CSS (si los tienes)
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            // Ejemplo para procesar imágenes (si las tienes)
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            //... otras reglas para otros tipos de archivos
-        ],
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-    ],
-    resolve: {
-        fallback: {
-            "buffer": require.resolve("buffer")
-        }
-    },
+	mode: "development", // o 'production' para el entorno de producción
+	entry: {
+		"dist/main.js": "./src/index.js",
+		"public/main.js": "./src/index.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "./"), // Ruta absoluta a la carpeta de salida
+		filename: "[name].js", // Nombre del archivo de salida
+		publicPath: "/", // Ruta pública para el navegador (importante para dev-server)
+	},
+
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "public"), // Ruta a la carpeta de archivos estáticos (index.html)
+			publicPath: "/", // La ruta pública para acceder a los archivos estáticos
+		},
+		port: 8080,
+		hot: true, // Habilita la recarga en caliente (opcional, pero recomendado)
+	},
+	module: {
+		// Para procesar diferentes tipos de archivos (CSS, imágenes, etc.)
+		rules: [
+			// Ejemplo para procesar archivos CSS (si los tienes)
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			// Ejemplo para procesar imágenes (si las tienes)
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			//... otras reglas para otros tipos de archivos
+		],
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			Buffer: ["buffer", "Buffer"],
+		}),
+	],
+	resolve: {
+		fallback: {
+			buffer: require.resolve("buffer"),
+		},
+	},
 };
